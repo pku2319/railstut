2970a8193bd071cbb80a2a14ddc859bd3a3064b2
Laura Ku <laura@coverhound.com>
2012-12-09 14:54:19 -0800

    Setting up the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstut::Application.config.secret_token = '55dfc2fd0d12e7fe1f2a84abcdf26ecf0b1d93857dcf10283861e1c5bf95fea37dc54bcbe6dbd62a142bd089852525b7daedc08ed14178561f8a21dc601011ef'
