37186cc8fe8aac37a4a8c6c62fecf814c542316c
Derek Chen <derekchen14@gmail.com>
2012-12-14 10:45:57 -0800

    Updated rspec tests for linking

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -28,4 +28,16 @@ describe "Static pages" do
     it { should have_selector('title', text: " | Contact") }
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+  end
+
 end
