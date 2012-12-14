14c355160f297dd12c2e3f006deeeb5897a960c4
Derek Chen <derekchen14@gmail.com>
2012-12-13 20:32:43 -0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Railstut::Application.routes.draw do
+  get "users/new" 
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
+  match '/signup',  to: 'users#new'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,26 +6,26 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path } 
     it { should have_selector('h1', text: 'Notable') }
-    it {page.should have_selector('title', text: "Railstut Sample App") }
-    it {page.should_not have_selector('title', text: " | Home") }
+    it { should have_selector('title', text: "Railstut Sample App") }
+    it { should_not have_selector('title', text: " | Home") }
   end
 
  describe "Help page" do
     before { visit help_path } 
-    it {page.should have_selector('h1', text: 'Help') }
-    it {page.should have_selector('title', text: " | Help") }
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: " | Help") }
   end
 
   describe "About page" do
     before { visit about_path } 
-    it {page.should have_selector('h1', text: 'About') }
-    it {page.should have_selector('title', text: " | About") }
+    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('title', text: " | About") }
   end
 
   describe "Contact page" do
     before { visit contact_path } 
-    it {page.should have_selector('h1', text: 'Contact') }
-    it {page.should have_selector('title', text: " | Contact") }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: " | Contact") }
   end
 
 end
