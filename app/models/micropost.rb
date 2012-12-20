716f89beacf40d9762df6b3c37996ebcaff5143f
Derek Chen <derekchen14@gmail.com>
2012-12-19 18:34:12 -0800

    Add other users to Micropost feed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,4 +9,12 @@ class Micropost < ActiveRecord::Base
   	presence: true,
   	length: { maximum: 140 }
 
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+          user_id: user.id)
+  end
+
+
 end
